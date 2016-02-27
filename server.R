8da01a416a96f009c3886a2e2ff4d4e26efad7fc
Dean <youngde@reed.edu>
2016-02-27 13:16:06 -0800

    updated ggvis tooltip to include nn rank

--- server.R
+++ server.R
@@ -103,46 +103,39 @@ shinyServer(function(input, output) {
     
   })
   
-  graph_data <- bind_rows(school.attr %>% mutate(Selected="Yes"), data.euclid %>% mutate(Selected="No")) %>%
-    arrange(desc(mathLImet))
+  graph_data <- bind_rows(school.attr %>% mutate(Selected="Yes"), data.euclid %>% mutate(Selected="No"))
   
-  graph_data %<>% mutate(rank = rownames(graph_data))
+  # nn = nearest neighbor rank
+  graph_data %<>% mutate(nn = as.numeric(rownames(graph_data)) - 1)
+  
+  # rank = pass rate rank
+  graph_data %<>% arrange(desc(mathLImet)) %>% mutate(rank = rownames(graph_data))
+  
   # order by pass rate
-  graph_data$School <- factor(graph_data$School, levels=graph_data$School[order(graph_data$mathLImet)])
+  graph_data$School <- factor(graph_data$School, levels=graph_data$School[order(desc(graph_data$mathLImet))])
   
   graph_data %>%
-    ggvis(~School,~mathLImet) %>%
+    ggvis(~School,~mathLImet, key := ~rank) %>%
     layer_bars(fill= ~Selected) %>%
-    layer_text(text := ~rank, fontWeight := "bold", fontSize := 20, align:="center", fill := "white") %>%
     hide_legend("fill") %>%
     set_options(height = 700, width = 1000) %>%
-    add_axis("x", title = "", properties = axis_props(labels=list(
-                                                      angle = -45,
-                                                      align = "right",
-                                                      baseline = "top",
-                                                      fill = "white"))) %>%
-    add_axis("y", title="L.I. Pass Rate (math) %", properties = axis_props(labels=list(fill = "white"))) %>%
-    add_tooltip(function(df) paste0("<b>", "<font color='red'>", "School: ", df$x, "<br>", "Pass Rate: ",
-                                    round(as.numeric(df$stack_upr_),2)),"hover") %>%
+    add_axis("x", properties = axis_props(labels=list(
+                                                      text = ""),
+                                          title=list(
+                                                      fill = "white")
+                                          )) %>%
+    add_axis("y", title="L.I. Pass Rate (math) %", 
+             properties = axis_props(labels=list(
+                                                  fill = "white"),
+                                     title=list(
+                                                  fill = "white")
+                                     )) %>%
+    add_tooltip(function(df) paste0("<b>", "<font color='black'>", "School: ", df$x, "<br>", 
+                                    "Pass Rate (PR): ",round(as.numeric(df$stack_upr_),2), "<br>", 
+                                    "PR Rank: ", df$rank, " out of ", dim(graph_data)[1],"<br>", 
+                                    "NN Rank: ", graph_data$nn[graph_data$rank == df$rank], " out of ", dim(graph_data)[1] - 1),
+                                    "hover") %>%
     bind_shiny("ggvis_plot","ggvis_plot_ui")
-  
-#   output$graph <- renderPlot({
-#     p <- ggplot(graph_data, aes(x=School,y=mathLImet, fill=Selected)) +
-#       geom_bar(stat="identity") +
-#       theme(legend.position="none", axis.text.x=element_text(angle=45, hjust=1)) +
-#       ggtitle(paste(isolate(input$k),"Nearest-Neighbor Schools")) +
-#       xlab("Schools") +
-#       ylab("L.I. Pass Rate % (math)") +
-#       scale_x_discrete(limits=levels(graph_data$School))
-#     
-#     p
-    
-    #plotly currently has difficulties with ordered factors
-    #ggplotly(p)
-    
-    #plot_ly(graph_data,x=School,y=mathLImet,type="bar",group=Selected,showlegend=FALSE)
-  # })
-  
   
   })
   
